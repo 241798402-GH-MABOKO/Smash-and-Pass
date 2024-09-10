f781449d074cb4e4d86bca98a7888c55e6d0f691
241798402-GH-MABOKO <hulisanigiven09@gmail.com>
2024-09-10 11:01:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Smash-and-Pass
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/241798402-GH-MABOKO/Smash-and-Pass)
---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('like').addEventListener('click', function() {
+  alert('You liked this profile!');
+  // Add functionality to handle "like" action
+});
+
+document.getElementById('dislike').addEventListener('click', function() {
+  alert('You disliked this profile!');
+  // Add functionality to handle "dislike" action
+});
+
